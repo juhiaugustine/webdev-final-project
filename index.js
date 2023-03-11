a830ffce5913607109bbd33383e76666f94c6dce
juhi <juh3@pdx.edu>
2023-03-11 15:01:47 -0800

    Added home, about me, projects and contact section

---
+++ index.js
@@ -0,0 +1,44 @@
+const header = document.querySelector("header");
+window.addEventListener("scroll", function() {
+    header.classList.toggle("sticky", window.scrollY > 100);
+});
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
