8676bf6ec0564a36d1f506ac48da23bdaa4df8ac
juhi <juh3@pdx.edu>
2023-03-12 00:05:34 -0800

    Removed accessibility warnings and validated code

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const header = document.querySelector("header");
+const menu = document.getElementById('menu-icon');
+const navlist = document.querySelector('.navlist');
+const form = document.getElementById('contact-us-form');
+
+function handleForm(event) { 
+    event.preventDefault(); 
+} 
+
+function submitContactUsForm() {
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let feedback = document.getElementById('feedback').value;
+    if(name != '' && email != '' && feedback != '') { 
+        var snackBar = document.getElementById('snackbar');
+        snackBar.className = 'show';
+        setTimeout(function(){ snackBar.className = snackBar.className.replace('show', ''); }, 3000);
+        form.reset();
+    }
+}
+
+form.addEventListener('submit', handleForm);
+
 window.addEventListener("scroll", function() {
     header.classList.toggle("sticky", window.scrollY > 100);
 });
-
-let menu = document.querySelector('#menu-icon');
-let navlist = document.querySelector('.navlist');
 
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
@@ -15,30 +34,3 @@ window.onscroll = () => {
     menu.classList.remove('bx-x');
     navlist.classList.remove('open');
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
- }
